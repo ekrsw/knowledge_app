772e53b03d1731b2b40a49dedf0c8f7d81ef30ed
ekrsw <eisuke.koresawa@gmail.com>
2025-07-09 21:20:32 +0900

    migrations

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Uuid
+from sqlalchemy import Boolean, String, Uuid
 from sqlalchemy.orm import Mapped, mapped_column
 import uuid
 
@@ -10,4 +10,8 @@ class User(Base):
 
     username: Mapped[str] = mapped_column(String, nullable=False, unique=True, index=True)
     email: Mapped[str] = mapped_column(String, unique=True, nullable=False, index=True)
+    full_name: Mapped[str] = mapped_column(String, nullable=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_sv: Mapped[bool] = mapped_column(Boolean, default=False)
     hashed_password: Mapped[str] = mapped_column(String, nullable=False)
